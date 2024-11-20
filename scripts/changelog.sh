8fbdaf85e45d4af94ad9c61ed01e6922c28fcdc2
Jan Vlnas <jan.vlnas@mews.com>
2024-11-20 21:21:49 +0100

    Correctly detect empty changelog

--- scripts/changelog.sh
+++ scripts/changelog.sh
@@ -8,13 +8,15 @@ set -euo pipefail
 : "${TMP_CHANGELOG_FILE:=tmp/oasdiff-changelog.md}"
 CHANGELOG_BACKUP_FILE="tmp/current-changelog.md"
 
-# tail here is to strip the inserted header
-oasdiff changelog "$OUTPUT_OAS_FILE" "$TMP_OAS_FILE" --format markup --attributes x-max-interval-in-months,x-restricted\
+# oasdiff always outputs a header line and some extra lines
+# so we need to skip the first line and trim empty lines to ensure an empty file
+oasdiff changelog "$OUTPUT_OAS_FILE" "$TMP_OAS_FILE" --format markup --attributes x-max-interval-in-months,x-restricted \
     | tail -n +2 > "$TMP_CHANGELOG_FILE"
 
-if [ ! -s "$TMP_CHANGELOG_FILE" ]; then
+# Check if the file is empty or contains only whitespace
+if [ ! -s "$TMP_CHANGELOG_FILE" ] || ! grep -q '[^[:space:]]' "$TMP_CHANGELOG_FILE"; then
     echo "No changes detected" >&2
-    exit 0
+    exit 1
 fi
 
 echo "Changes detected" >&2
@@ -26,5 +28,6 @@ if [ -f "$OUTPUT_CHANGELOG_FILE" ]; then
 else
     touch "$CHANGELOG_BACKUP_FILE"
 fi
+
 # 2. create new changelog by combining: current date, new changes, and old changelog
 echo -e "# $(date +"%Y-%m-%d")\n" | cat - "$TMP_CHANGELOG_FILE" "$CHANGELOG_BACKUP_FILE" > "$OUTPUT_CHANGELOG_FILE"
